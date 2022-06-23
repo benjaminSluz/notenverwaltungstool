a97f9e134daaf066cee11f230b017f847fd7aaec
Benjamin Schleiss <Benjamin_Schleiss@sluz.ch>
2022-06-23 14:34:11 +0200

    some polishing

--- src/views/NotenverwaltungView.vue
+++ src/views/NotenverwaltungView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <ul id="Fächer">
       <li v-for="(noten, fach) in Fächer" :key="fach">
-        {{ fach }}
+        <span class="Fach"> {{ fach }}</span>
         <ul>
           <li v-for="(note, index) in noten.noten" :key="index">
             <span>
@@ -11,8 +11,11 @@
             </span>
           </li>
           <div>
-            <span>{{ avgNote(noten.noten) }}</span>
-            <input type="number" @keyup.enter="addNote($event, fach)" />
+            <div>Abschluss Note:{{ avgNote(noten.noten) }}</div>
+            <label>
+              Neue Note einfügen:
+              <input type="number" @keyup.enter="addNote($event, fach)" />
+            </label>
           </div>
         </ul>
       </li>
@@ -31,9 +34,9 @@ const Fächer = ref({
   M153: { noten: [6, 6] },
   M306: { noten: [6, 6] },
   NWS: { noten: [6, 6] },
-  SPK: { noten: [] },
-  SPO: { noten: [] },
-  WUR: { noten: [] },
+  SPK: { noten: [4.24, 5.7] },
+  SPO: { noten: [4.8, 3.21] },
+  WUR: { noten: [6, 2.4] },
 });
 function deleteNote(fach, note) {
   console.log(Fächer.value[fach].noten, fach, note);
@@ -64,6 +67,9 @@ function avgNote(noten) {
   min-width: 100vw;
   margin: 0;
   justify-content: center;
+}
+.Fach {
+  font-weight: bolder;
 }
 #Fächer {
   border: black 1px solid;
